197815fefe3578cbd02d800e90fdd75c952fe52a
Jay Berdimuratov <terexberd@yahoo.com>
2018-06-26 12:16:39 -0400

    Update gold_rush_test.rb

--- gold_rush_test.rb
+++ gold_rush_test.rb
@@ -12,6 +12,7 @@ class GoldRushTest < MiniTest::Test
   	@gold_rush = GoldRush::new
   end
 
+  # UNIT TESTS FOR GoldRush
   # Verify that creating a GoldRush is an instance of GoldRush and not
   # something else.
   def test_gold_rush_is_gold_rush
@@ -24,6 +25,7 @@ class GoldRushTest < MiniTest::Test
     refute_nil @gold_rush
   end
 
+  # UNIT TESTS FOR METHOD initialize
   # Verifies the names for the locations
   def test_fun_city_locs
 	@city = GoldRush::new
@@ -43,6 +45,12 @@ class GoldRushTest < MiniTest::Test
 	assert_equal nil, @city.sutter.forward_path3
 	assert_equal nil, @city.nevada.forward_path2
   end
+	
+  # Verifies that the prospectors start off with 0 gold and silver
+  def test_amount_gold_silver
+	assert_equal 0, @gold_rush.prospector.num_gold
+	assert_equal 0, @gold_rush.prospector.num_silver
+  end
 
   # Verifies that there are 5 prospectors
   def test_fun_five_prospectors
@@ -51,6 +59,7 @@ class GoldRushTest < MiniTest::Test
 	assert_equal 5, @city.prospectors.length
   end
 
+  # UNIT TESTS FOR METHOD argchecker
   # Verifies that the program aborts upon finding out there are no arguments
   def test_no_arg
 	assert_output(nil, abort) {GoldRush.argchecker()}
@@ -62,17 +71,13 @@ class GoldRushTest < MiniTest::Test
 	assert_output(nil, abort) {GoldRush.argchecker(-1)}
   end
 
-  # Verifies that the prospectors start off with 0 gold and silver
-  def test_amount_gold_silver
-	assert_equal 0, @gold_rush.prospector.num_gold
-	assert_equal 0, @gold_rush.prospector.num_silver
-  end
-
+  # UNIT TESTS FOR METHOD set_location
   # Verifies that the initial location of all prospectors is Sutter Creek
   def test_initial_location
 	assert_equal "Sutter Creek", @gold_rush.set_location
   end
 
+  # UNIT TESTS FOR METHOD test_calculate
   # Verifies that calculate method returns the right amount of money
   def test_calculate
   	assert_equal '508.16', @gold_rush.calculate(23, 25)
@@ -84,6 +89,7 @@ class GoldRushTest < MiniTest::Test
   	assert_equal '0', @gold_rush.calculate(-1, 0)
   end
 
+  # UNIT TESTS FOR NUM_GOLD and NUM_SILVER SETTERS
   # Verifies that the numbers for gold and silver can be set
   def test_current_gold_silver
   	@gold_rush.prospector.num_gold = 2
@@ -92,6 +98,7 @@ class GoldRushTest < MiniTest::Test
   	assert_equal 2, @gold_rush.prospector.num_silver
   end
   
+  # UNIT TESTS FOR METHOD print_location
   # Verifies that location print method works
   def test_print_location
   	@gold_rush.initialize(2, 2)
@@ -104,7 +111,8 @@ class GoldRushTest < MiniTest::Test
   	@gold_rush.initialize(2, 2)
   	assert_equal "Error", @gold_rush.print_location(@prospectors[0], -2)
   end
-
+	
+  # UNIT TESTS FOR METHOD way_generator
   # Verifies that the way generator returns 1
   def test_way_generator
   	assert_equal 1, @gold_rush.way_generator(1)
