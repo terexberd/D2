4e5d683ceb77b916aba146abfe1c4b4d0c88fb97
Jay Berdimuratov <terexberd@yahoo.com>
2018-06-26 11:56:39 -0400

    Rubocop

--- prospector.rb
+++ prospector.rb
@@ -1,9 +1,10 @@
+# Prospector Class
 class Prospector
-	#Create new driver
-	attr_accessor :prospector_name, :num_gold, :num_silver
-	def initialize(prospector_name, num_gold, num_silver)
-		@prospector_name = prospector_name
-		@num_gold = num_gold
-		@num_silver = num_silver
-	end
+  # Create new prospector
+  attr_accessor :prospector_name, :num_gold, :num_silver
+  def initialize(prospector_name, num_gold, num_silver)
+    @prospector_name = prospector_name
+    @num_gold = num_gold
+    @num_silver = num_silver
+  end
 end
